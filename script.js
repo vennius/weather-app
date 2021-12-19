4def04d6b81ee1037b241c8e43042a8ce01b5ffa
SCxoneZ <89004475+SCxoneZ@users.noreply.github.com>
2021-12-19 10:26:10 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-//http://api.openweathermap.org/data/2.5/weather?q=medan&units=metric&appid=681f35b64d6e9f95a76677364a4f78b0
-
 const url = (city) => `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=681f35b64d6e9f95a76677364a4f78b0`;
-
 const translate = (word) => `https://scxonez-api.herokuapp.com/api/other/translate?kata=${word}&apikey=Alphabot`;
 
 const getData = (url) => {
@@ -11,35 +8,50 @@ const getData = (url) => {
     .catch(err => err);
 };
 
-const body = document.querySelector('body');
 const input = document.querySelector('.input input');
 const img = document.querySelectorAll('.deg img');
 const deg = document.querySelector('.deg-value');
 const status = document.querySelector('.status p');
 
+//Default
+def();
+
 input.addEventListener('change', async () => {
   const data = await getData(url(input.value));
   const translated = await getData(translate(data.weather[0].description));
-  const currentDeg = data.main.temp;
-  console.log(data);
-  console.log(currentDeg);
-  console.log(translated);
+  const currentDeg = parseInt(data.main.temp);
   img.forEach((img) => {
     img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   });
   deg.textContent = data.main.temp + '°C';
   status.innerHTML = translated.result.text.toUpperCase();
-  
-  if(currentDeg >= 30){
-    body.backgroundColor = 'red';
-  }else if(currentDeg < 30){
-    body.backgroundColor = 'green';
-  }else if(currentDeg < 25 && currentDeg >= 20){
-    body.backgroundColor = 'salmon';
-  }else if(currentDeg < 20 && currentDeg <= 10){
-    body.backgroundColor = 'magenta';
-  }else if(currentDeg < 10){
-    body.backgroundColor = 'orange';
-  }
-  
+  systemColor();
 });
+
+async function def(){
+  const data = await getData(url('Medan'));
+  const translated = await getData(translate(data.weather[0].description));
+  const currentDeg = parseInt(data.main.temp);
+  img.forEach((img) => {
+    img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  });
+  deg.textContent = data.main.temp + '°C';
+  status.innerHTML = translated.result.text.toUpperCase();
+  systemColor();
+}
+
+function systemColor(){
+  if(currentDeg > 30){
+     document.body.style.backgroundColor = 'salmon';
+   }else if(currentDeg > 25){
+     document.body.style.backgroundColor = '#72faa6';
+   }else if(currentDeg > 20){
+     document.body.style.backgroundColor = '#89effc';
+   }else if(currentDeg > 10){
+     document.body.style.backgroundColor = '#ebe19c';
+   }else if(currentDeg > 0){
+     document.body.style.backgroundColor = '#cef4ff';
+   }else if(currentDeg < 0){
+     document.body.style.backgroundColor = '#7d7d7d';
+   }
+}
