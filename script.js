6b1786e8875a304999dd837cc0b15678bbbd74ad
SCxoneZ <89004475+SCxoneZ@users.noreply.github.com>
2021-12-21 15:55:53 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const url = (city) => `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=681f35b64d6e9f95a76677364a4f78b0`;
-const translate = (word) => `https://scxonez-api.herokuapp.com/api/other/translate?kata=${word}&apikey=Alphabot`;
+//const translate = (word) => `https://scxonez-api.herokuapp.com/api/other/translate?kata=${word}&apikey=Alphabot`;
 
 const getData = (url) => {
   return fetch(url)
@@ -8,11 +8,12 @@ const getData = (url) => {
     .catch(err => err);
 };
 
+
+const body = document.querySelector('body');
 const input = document.querySelector('.input input');
 const img = document.querySelectorAll('.deg img');
 const deg = document.querySelector('.deg-value');
 const status = document.querySelector('.status p');
-
 let currentDeg = 0;
 
 //Default
@@ -20,13 +21,13 @@ def();
 
 input.addEventListener('change', async () => {
   const data = await getData(url(input.value));
-  const translated = await getData(translate(data.weather[0].description));
+  //const translated = await getData(translate(data.weather[0].description));
   currentDeg = parseInt(data.main.temp);
   img.forEach((img) => {
     img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   });
   deg.textContent = data.main.temp + '°C';
-  status.innerHTML = translated.result.text.toUpperCase();
+  status.innerHTML = data.weather[0].description.toUpperCase();
   systemColor();
 });
 
@@ -40,11 +41,12 @@ async function def(){
   deg.textContent = data.main.temp + '°C';
   status.innerHTML = translated.result.text.toUpperCase();
   systemColor();
+  input.value = 'Medan';
 }
 
 function systemColor(){
   if(currentDeg > 30){
-     document.body.style.backgroundColor = 'salmon';
+     body.style.backgroundColor = 'salmon';
    }else if(currentDeg > 25){
      document.body.style.backgroundColor = '#72faa6';
    }else if(currentDeg > 20){
@@ -55,5 +57,5 @@ function systemColor(){
      document.body.style.backgroundColor = '#cef4ff';
    }else if(currentDeg < 0){
      document.body.style.backgroundColor = '#7d7d7d';
-   }
+}
 }
